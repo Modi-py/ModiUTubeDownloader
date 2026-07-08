869c17bdfd03bfdd49e131dbf8a03c5cb17084ba
Modi-py <shmuelshiurim@gmail.com>
2026-07-08 20:29:26 +0300

    The py file is getting downloaded from setup.bat
    Since I want to share the setup.bat file with others, I didn't want to
    send more than one file, so now the "Download the links.py" file is
    getting downloaded automatically from the repo on GitHub, to the user's
    Desktop.

--- Setup.bat
+++ Setup.bat
@@ -42,6 +42,23 @@ attrib +s +h "%DESKTOP_DIR%\Audio_Downloads\desktop.ini"
 attrib +r "%DESKTOP_DIR%\Video_Downloads"
 attrib +r "%DESKTOP_DIR%\Audio_Downloads"
 
+
+:: --- DOWNLOAD SCRIPT FROM GITHUB ---
+echo Downloading 'Download the links.py' to Desktop...
+
+:: Replace the URL below with your actual raw GitHub link
+set "GITHUB_URL=https://raw.githubusercontent.com/Modi-py/ModiUTubeDownloader/main/Download_the_links.py"
+
+:: Download the file to the Desktop
+curl -L -o "%DESKTOP_DIR%\Download_the_links.py" "%GITHUB_URL%"
+
+if exist "%DESKTOP_DIR%\Download_the_links.py" (
+    echo Download successful!
+) else (
+    echo ERROR: Failed to download the script. Please check the URL.
+)
+
+
 echo ========================================================
 echo Creating configuration files with exact content...
 echo ========================================================
